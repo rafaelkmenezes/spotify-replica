2b75a95c1edd86f4a60e79b32695b46c76c64c28
rafaelkmenezes <rafaelkmenezes@gmail.com>
2025-06-06 16:18:41 -0300

    novos ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,8 +4,9 @@ import axios from "axios";
 
 // const NODE_ENV = process.env.NODE_ENV;
 // OU
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3000/api";
+const URL = "https://spotify-replica-i7i4.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
