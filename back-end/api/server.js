a8ea9b6c48ac3e799d15669867d171209898c9ff
rafaelkmenezes <rafaelkmenezes@gmail.com>
2025-06-06 15:57:06 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -5,6 +5,11 @@
 import express from "express";
 import cors from "cors"; //Middleware para resolver problemas de domínios diferentes. No caso, de portas diferentes
 import { database } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
+
+console.log(__dirname);
 
 const app = express();
 const PORT = 3000;
@@ -12,20 +17,30 @@ const PORT = 3000;
 app.use(cors());
 // app.use(express.json()); //Usado em requisições tipo POST que transforma em JSON e consegue ler em JS
 
-app.listen(PORT, () => {
-  console.log(`Servidor está escutando na porta ${PORT}.`);
+app.get("/api/", (request, response) => {
+  response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/", (request, response) => {
-  response.send(
-    "Olá, Mundo! Agora não preciso ficar reiniciando o servidor toda vez que mudar o arquivo. A instrução 'node --watch ./server.js' reinicia toda vez que o arquivo é alterado."
-  );
-});
-
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await database.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await database.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+console.log(path.join(__dirname, "../front-end/dist"));
+
+app.get("/*splat", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
+// app.get("*", async (request, response) => {
+//   response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+// });
+console.log(path.join(path.join(__dirname, "../front-end/dist/index.html")));
+
+app.listen(PORT, () => {
+  console.log(`Servidor está escutando na porta ${PORT}.`);
 });
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,11 @@
 //Fetch (nativo JS) ou Axios (não nativo)
+import "dotenv/config";
 import axios from "axios";
 
-const URL = "http://localhost:3000";
+// const NODE_ENV = process.env.NODE_ENV;
+// OU
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
